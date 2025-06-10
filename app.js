d7b9ca63dec6ad5cb43c43dd1132b5272b593a52
Fiso <youn110158@naver.com>
2025-06-10 21:39:08 +0900

    테스트 로직 커밋

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
     res.send("Hello, Express");
 });
 
+// testController 불러오기
+const testController = require("./src/controllers/testController");
+
+// 테스트용 라우트 추가
+app.get("/test", testController);
+
 app.listen(app.get("port"), () => {
     console.log(app.get("port"), "번 포트에서 대기 중");
 });
