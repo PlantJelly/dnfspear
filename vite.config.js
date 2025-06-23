2a61b0cc8a76b71fd21ba70131c3ba441783b251
Fiso <youn110158@naver.com>
2025-06-23 21:23:43 +0900

    포트 기본 세팅

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import devtoolsJson from "vite-plugin-devtools-json";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.PORT);
 
 export default defineConfig({
     plugins: [sveltekit(), devtoolsJson()],
+    server: {
+        port: Number(process.env.PORT) ?? 5173,
+    },
 });
