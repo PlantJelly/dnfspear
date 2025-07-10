d563763ca20d7ad382ef027559bff059c2c9d0a2
PlantJelly <rmfprm2@naver.com>
2025-07-10 18:47:26 +0900

    tailwindcss 설치

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import devtoolsJson from "vite-plugin-devtools-json";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
+import tailwindcss from "@tailwindcss/vite";
 
 dotenv.config();
 
 console.log(process.env.PORT);
 
 export default defineConfig({
-    plugins: [sveltekit(), devtoolsJson()],
+    plugins: [sveltekit(), devtoolsJson(), tailwindcss()],
     server: {
         port: Number(process.env.PORT) ?? 5173,
     },
